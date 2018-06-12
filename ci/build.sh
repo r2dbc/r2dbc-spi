11d961617a74ef84865ff9c3e1ef08d10f794efd
Ben Hale <bhale@pivotal.io>
2018-06-12 14:05:34 -0700

    Polishing

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-repository=$(pwd)/distribution-repository
+repository=$(pwd)/r2dbc-spi-artifactory
 
 cd r2dbc-spi
 ./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
