005e79f6788c1f026b961df54d68eff93ed6bb9d
Ben Hale <bhale@pivotal.io>
2018-06-20 14:49:34 +0100

    Cleanup Maven Repository

--- ci/build.sh
+++ ci/build.sh
@@ -5,5 +5,7 @@
 
 repository=$(pwd)/r2dbc-spi-artifactory
 
+rm -rf $HOME/.m2/repository/io/r2dbc
+
 cd r2dbc-spi
 ./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
