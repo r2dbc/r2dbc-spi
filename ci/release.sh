cec744ecd956aae88ebcda1db342c734aa0c9104
Ben Hale <bhale@pivotal.io>
2018-09-22 17:38:30 -0700

    Release scripts

---
+++ ci/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e -u
+
+[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
+
+r2dbc_spi_artifactory=$(pwd)/r2dbc-spi-artifactory
+
+rm -rf $HOME/.m2/repository/io/r2dbc 2> /dev/null || :
+
+cd r2dbc-spi
+./mvnw deploy \
+    -DaltDeploymentRepository=distribution::default::file://${r2dbc_spi_artifactory}
