34f36439032c3200199e632ba332a685c02e317c
Mark Paluch <mpaluch@pivotal.io>
2019-01-24 16:16:40 +0100

    Extend ColumnMetadata
    ColumnMetadata now exposes additional metadata information on a best-effort
    basis. Drivers are free to provide metadata if it is available as part of a
    result response or statically available.
    
    Drivers also can decide which methods to implement and they can expose a
    native metadata object if they can expose additional metadata.
    
    [resolves #42]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- r2dbc-spi/src/main/java/io/r2dbc/spi/RowMetadata.java
+++ r2dbc-spi/src/main/java/io/r2dbc/spi/RowMetadata.java
@@ -16,6 +16,8 @@
 
 package io.r2dbc.spi;
 
+import java.util.NoSuchElementException;
+
 /**
  * Represents the metadata for a row of the results returned from a query.
  */
@@ -26,7 +28,9 @@ public interface RowMetadata {
      *
      * @param identifier the identifier of the column
      * @return the {@link ColumnMetadata} for one column in this row
-     * @throws IllegalArgumentException if {@code identifier} is {@code null}
+     * @throws IllegalArgumentException       if {@code identifier} is {@code null} or not supported
+     * @throws NoSuchElementException         if there is no column with the name {@code identifier}
+     * @throws ArrayIndexOutOfBoundsException if the {@code identifier} is a {@link Integer index} and it is less than zero or greater than the number of available columns.
      */
     ColumnMetadata getColumnMetadata(Object identifier);
 
