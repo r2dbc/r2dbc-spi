5e26dba579d9901b7407ed76e574cfdd08184bf4
Mark Paluch <mpaluch@pivotal.io>
2019-01-25 11:54:00 +0100

    Document behavior for absent columns and invalid identifiers
    RowMetadata.getColumnMetadata(…) may throw IllegalArgumentException
    if the identifier is not supported (i.e. not a name, not a index).
    
    NoSuchElementException may be thrown to indicate the column is not
    available in the Row.
    
    ArrayIndexOutOfBoundsException may be thrown if the index is negative
    or exceeds the column count.

--- r2dbc-spi/src/main/java/io/r2dbc/spi/RowMetadata.java
+++ r2dbc-spi/src/main/java/io/r2dbc/spi/RowMetadata.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package io.r2dbc.spi;
 
+import java.util.NoSuchElementException;
+
 /**
  * Represents the metadata for a row of the results returned from a query.
  */
@@ -26,7 +28,9 @@ public interface RowMetadata {
      *
      * @param identifier the identifier of the column
      * @return the {@link ColumnMetadata} for one column in this row
-     * @throws IllegalArgumentException if {@code identifier} is {@code null}
+     * @throws IllegalArgumentException       if {@code identifier} is {@code null} or not supported
+     * @throws NoSuchElementException         if there is no column with the name {@code identifier}
+     * @throws ArrayIndexOutOfBoundsException if the {@code identifier} is a {@link Integer index} and it is less than zero or greater than the number of available columns.
      */
     ColumnMetadata getColumnMetadata(Object identifier);
 
