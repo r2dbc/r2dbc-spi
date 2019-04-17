b4a6b25b0437969a814412b0a868efafa58178ff
Ben Hale <bhale@pivotal.io>
2019-04-17 11:09:24 -0700

    BLOB/CLOB Test Improvements
    Previously the BLOB and CLOB tests assumed BLOB and CLOB were supported data
    types in all databases.  This is not true, specifically for PostgreSQL.  This
    change makes those types configurable and makes some general improvements to
    the tests themselves to use JDBC as an independent check on the R2DBC
    implementations.
    
    In addition MockBlob and MockClob types were introduced to help ensure that
    discard() was called.
    
    [#41]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ r2dbc-spi-test/src/main/java/io/r2dbc/spi/test/MockBlob.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2017-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.spi.test;
+
+import io.r2dbc.spi.Blob;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public final class MockBlob implements Blob {
+
+    private final Flux<ByteBuffer> stream;
+
+    private boolean discardCalled = false;
+
+    private MockBlob(Flux<ByteBuffer> stream) {
+        this.stream = Assert.requireNonNull(stream, "stream must not be null");
+    }
+
+    public static MockBlob.Builder builder() {
+        return new MockBlob.Builder();
+    }
+
+    public static MockBlob empty() {
+        return builder().build();
+    }
+
+    @Override
+    public Publisher<Void> discard() {
+        this.discardCalled = true;
+        return Mono.empty();
+    }
+
+    public boolean isDiscardCalled() {
+        return this.discardCalled;
+    }
+
+    @Override
+    public Publisher<ByteBuffer> stream() {
+        return this.stream;
+    }
+
+    @Override
+    public String toString() {
+        return "MockBlob{" +
+            "stream=" + this.stream +
+            ", discardCalled=" + this.discardCalled +
+            '}';
+    }
+
+    public static final class Builder {
+
+        private final List<ByteBuffer> items = new ArrayList<>();
+
+        private Builder() {
+        }
+
+        public MockBlob build() {
+            return new MockBlob(Flux.fromIterable(this.items));
+        }
+
+        public MockBlob.Builder item(ByteBuffer... items) {
+            Assert.requireNonNull(items, "items must not be null");
+
+            Stream.of(items)
+                .peek(item -> Assert.requireNonNull(item, "item must not be null"))
+                .forEach(this.items::add);
+
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                "items=" + this.items +
+                '}';
+        }
+    }
+}
