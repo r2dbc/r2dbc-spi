6015e608f5e3529383296fb5582de496d09f6cb7
Mark Paluch <mpaluch@pivotal.io>
2019-05-23 11:30:34 +0200

    Add specification for Closeable.
    We now provide a Closeable interface as optional extension
    for stateful resources such as connection pools that typically implement
    a ConnectionFactory interface.
    
    As part of this change, we also make Connection extending Closeable.

---
+++ r2dbc-spi/src/main/java/io/r2dbc/spi/Closeable.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.spi;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+/**
+ * A {@link Closeable} is an object that can be closed.  The {@link #close()} method is invoked to release resources that the object is holding (such as open connections).
+ */
+@FunctionalInterface
+public interface Closeable {
+
+    /**
+     * Close this object and release any resources associated with it.  If the object is already closed, then {@link Publisher#subscribe(Subscriber) subscriptions} complete successfully and the
+     * close operation has no effect.
+     *
+     * @return a {@link Publisher} that termination is complete
+     */
+    Publisher<Void> close();
+
+}
