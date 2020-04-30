4467b91804decbe87140f41f6e1db344afa4ec3b
Mark Paluch <mpaluch@pivotal.io>
2020-04-30 09:53:19 +0200

    Polishing
    Add this for field dereference.
    
    [#166][#169]

--- r2dbc-spi/src/main/java/io/r2dbc/spi/Option.java
+++ r2dbc-spi/src/main/java/io/r2dbc/spi/Option.java
@@ -90,23 +90,27 @@ public final class Option<T> {
     @Override
     public String toString() {
         return "Option{" +
-            "name='" + name + '\'' +
-            ", sensitive=" + sensitive +
+            "name='" + this.name + '\'' +
+            ", sensitive=" + this.sensitive +
             '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Option<?> option = (Option<?>) o;
-        return sensitive == option.sensitive &&
-                name.equals(option.name);
+        return this.sensitive == option.sensitive &&
+            this.name.equals(option.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, sensitive);
+        return Objects.hash(this.name, this.sensitive);
     }
 
     boolean sensitive() {
