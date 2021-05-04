3136fcfe1ce9fbbda2b5aec057461c738e56a125
Mark Paluch <mpaluch@vmware.com>
2021-05-04 15:52:02 +0200

    Adapt to review discussion.
    Signed-off-by: Mark Paluch <mpaluch@vmware.com>

--- r2dbc-spi/src/main/java/io/r2dbc/spi/OutParametersMetadata.java
+++ r2dbc-spi/src/main/java/io/r2dbc/spi/OutParametersMetadata.java
@@ -16,56 +16,40 @@
 
 package io.r2dbc.spi;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
  * Represents the metadata for {@code OUT} parameters of the results returned from a stored procedure.
- * Metadata for parameters can be either retrieved by specifying a parameter name or the parameter index.
+ * Metadata for parameters can be either retrieved by specifying a out parameter name or the out parameter index.
  * Parameter indexes are {@code 0}-based.
  */
 public interface OutParametersMetadata {
 
     /**
-     * Returns the {@link OutParameterMetadata} for one parameter.
+     * Returns the {@link OutParameterMetadata} for one out parameter.
      *
-     * @param index the parameter index starting at 0
-     * @return the {@link OutParameterMetadata} for one parameter
-     * @throws ArrayIndexOutOfBoundsException if the {@code index} is less than zero or greater than the number of available parameters.
+     * @param index the out parameter index starting at 0
+     * @return the {@link OutParameterMetadata} for one out parameter
+     * @throws ArrayIndexOutOfBoundsException if the {@code index} is less than zero or greater than the number of available out parameters.
      */
     OutParameterMetadata getParameterMetadata(int index);
 
     /**
      * Returns the {@link OutParameterMetadata} for one parameter.
      *
-     * @param name the name of the parameter.  Parameter names are case insensitive.  When a get method contains several parameters with same name, then the value of the first matching parameter
-     *             will be returned
-     * @return the {@link OutParameterMetadata} for one parameter
+     * @param name the name of the out parameter.  Parameter names are case insensitive.
+     * @return the {@link OutParameterMetadata} for one out parameter
      * @throws IllegalArgumentException if {@code name} is {@code null}
-     * @throws NoSuchElementException   if there is no parameter with the {@code name}
+     * @throws NoSuchElementException   if there is no out parameter with the {@code name}
      */
     OutParameterMetadata getParameterMetadata(String name);
 
     /**
-     * Returns the {@link OutParameterMetadata} for all parameters.
+     * Returns the {@link OutParameterMetadata} for all out parameters.
      *
-     * @return the {@link OutParameterMetadata} for all parameters
+     * @return the {@link OutParameterMetadata} for all out parameters
      */
-    Iterable<? extends OutParameterMetadata> getParameterMetadatas();
-
-    /**
-     * Returns an unmodifiable collection of parameter names.
-     * <p>
-     * Any attempts to modify the returned collection, whether direct or via its iterator, result in an {@link UnsupportedOperationException}.
-     * <p>
-     * The iteration order of the parameter names depends on the actual stored procedure.
-     * Parameter names may appear multiple times if the result specifies multiple parameters with the same name.
-     * Lookups through {@link Collection#contains(Object)} are case-insensitive.
-     * Drivers may enhance comparison sorting rules with escape characters to enforce a particular mode of comparison
-     * when querying for presence/absence of a parameter.
-     *
-     * @return the parameter names.
-     */
-    Collection<String> getParameterNames();
+    List<? extends OutParameterMetadata> getParameterMetadatas();
 
 }
