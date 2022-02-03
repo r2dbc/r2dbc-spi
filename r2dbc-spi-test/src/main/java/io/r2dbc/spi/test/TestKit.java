80b6ece5819fbbc560ed0019be4425b2a6a72c4a
diego Dupin <diego.dupinmariadb.com>
2022-02-03 14:04:39 +0100

    Fix `TestKit` encoding where `ByteBuffer.array` may not exactly represent UTF8 encoded bytes.
    backing array ByteBuffer.array may not exactly represent UTF8 encoded bytes.
    PR permit to ensure test is valid whatever the JVM is used
    
    Example using jdk8:
    
    "test-value".getBytes(StandardCharsets.UTF_8) => 0x746573742D76616C7565
    StandardCharsets.UTF_8.encode("test-value").array() => 0x746573742D76616C756500 having an additional ending 0x00
    Signed-off-by: diego Dupin <diego.dupin@gmail.com>
    
    [resolves #264]
    
    Signed-off-by: Mark Paluch <mpaluch@vmware.com>

--- r2dbc-spi-test/src/main/java/io/r2dbc/spi/test/TestKit.java
+++ r2dbc-spi-test/src/main/java/io/r2dbc/spi/test/TestKit.java
@@ -379,7 +379,7 @@ public interface TestKit<T> {
 
             @Override
             protected void setValues(PreparedStatement ps, LobCreator lobCreator) throws SQLException {
-                lobCreator.setBlobAsBytes(ps, 1, StandardCharsets.UTF_8.encode("test-value").array());
+                lobCreator.setBlobAsBytes(ps, 1, "test-value".getBytes(StandardCharsets.UTF_8));
             }
 
         });
@@ -401,7 +401,7 @@ public interface TestKit<T> {
                 Connection::close)
             .as(StepVerifier::create)
             .expectNextMatches(actual -> {
-                ByteBuffer expected = StandardCharsets.UTF_8.encode("test-value");
+                ByteBuffer expected = ByteBuffer.wrap("test-value".getBytes(StandardCharsets.UTF_8));
                 return Arrays.equals(expected.array(), actual);
             })
             .verifyComplete();
@@ -420,7 +420,7 @@ public interface TestKit<T> {
             .as(StepVerifier::create)
             .expectNextMatches(actual -> {
                 ByteBuffer expected = StandardCharsets.UTF_8.encode("test-value");
-                return Arrays.equals(expected.array(), actual.array());
+                return actual.compareTo(expected) == 0;
             })
             .verifyComplete();
     }
